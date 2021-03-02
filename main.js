032b3cf968589a9ec82421facfc047399849f709
Clifford Morin <cemorin21@gmail.com>
2021-03-02 15:15:15 -0500

    adding scores

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const timer = document.getElementById('timer')
 
 let timeLeft = 120
 let shuffledQuestions, currentQuestionIndex, timeInterval
+// let countRightAnswers = 0
 
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
@@ -16,16 +17,23 @@ nextButton.addEventListener('click', () => {
     setNextQuestion()
 })
 
+//Start Game on click of Start btn
 function startGame() {
   console.log('Started');
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  //shuffles questions randomly
+  shuffledQuestions = questions.sort(() => Math.random() - .5) //shuffled questions = question array which is sorted by a random number. If sort is a negative or a positive number it sorts it differently but not randomly so we use Math.random which gives a number between 1 and 0. We subtract .5 from it to we can get a number that is less than zero or above zero 50% of the time. 
   currentQuestionIndex = 0
   home.classList.add('hide')
   quiz.classList.remove('hide')
+
+  //timer set and starts counting down from 120
   startTimer()
+
+  //creates the question
   setNextQuestion()
 }
 
+//Timer function. 
 function startTimer() {
     timeInterval = setInterval(() => {
         if (timeLeft > 1) {
@@ -40,6 +48,7 @@ function startTimer() {
     }, 1000);
 }
 
+//resets background and then sets next random question 
 function setNextQuestion() {
   resetState()
   showQuestion(shuffledQuestions[currentQuestionIndex])
@@ -59,25 +68,30 @@ function showQuestion(question) {
     })
 }
 
+//Resets quiz to the default state
 function resetState() {
     clearStatusClass(document.querySelector('main'))
-    //add when taken to finished section resets 
-    //add restartBtn here to reset to home screen
     nextButton.classList.add('hide')
+    //removes the default answer buttons so we can replace them with the answers in each question
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild
         (answerButtonsElement.firstChild)
     }
 }
 
+// Select answer
 function selectAnswer(e) {
+    //which button we selected
   var selectedButton = e.target
+  //check if that button is correct
   var correct = selectedButton.dataset.correct
   setStatusClass(document.querySelector('main'), correct)
   //if not correct subtract 20 seconds from timer
   if (!correct) {
   timeLeft -= 20
   }
+  //loops through all other answer buttons and checks to see if they are correct.
+  //answerButtons.children returns a live collection which is not an array. So we need to convert it to an array so we can use the forEach loop.
   Array.from(answerButtonsElement.children).forEach(button => {
       setStatusClass(button, button.dataset.correct)
   })
@@ -88,6 +102,7 @@ function selectAnswer(e) {
   }
 }
 
+//clears elements status and checks if it correct or wrong then changes the class based on that.
 function setStatusClass(element, correct) {
     clearStatusClass(element)
     if (correct) {
@@ -97,6 +112,7 @@ function setStatusClass(element, correct) {
     }
 }
 
+//function to clear element of class
 function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
